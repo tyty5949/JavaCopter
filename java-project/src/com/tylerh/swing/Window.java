3b2e7975517b28960cdc000d380792bde3e3fab6
tsh5949 <tyty5949@gmail.com>
2016-03-21 09:11:39 -0500

    School commit for control program

--- java-project/src/com/tylerh/swing/Window.java
+++ java-project/src/com/tylerh/swing/Window.java
@@ -3,7 +3,11 @@ package com.tylerh.swing;
 import com.tylerh.Main;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 /**
  * Created by tsh5949 on 3/8/2016.
@@ -25,7 +29,89 @@ public class Window implements Runnable {
         frame.setSize(200, 634);
         frame.setLocation((int) (Main.screenWidth / 2) + 200, (int) (Main.screenHeight / 2) - 328);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setBackground(Color.WHITE);
+        frame.add(new WindowPanel());
         frame.setVisible(true);
+    }
+
+    public class WindowPanel extends JPanel {
+
+        private int tempPitch;
+        private int tempYaw;
+
+        private JSlider throttle;
+        private JSlider pitch;
+        private JSlider yaw;
+
+        private JLabel throttleLabel;
+        private JLabel pitchLabel;
+        private JLabel yawLabel;
+
+        public WindowPanel() {
+            //setBackground(Color.WHITE);
+            setLayout(null);
+
+            throttle = new JSlider(JSlider.HORIZONTAL,0 , 100, 0);
+            throttle.addChangeListener(new ChangeListener() {
+                @Override
+                public void stateChanged(ChangeEvent e) {
+                    JSlider source = (JSlider)e.getSource();
+                    System.out.println(source.getValue());
+                }
+            });
+            throttle.setMajorTickSpacing(20);
+            throttle.setMinorTickSpacing(10);
+            throttle.setPaintTicks(true);
+            throttle.setPaintLabels(true);
+            throttle.setBounds(10, 20, 160, 50);
+            add(throttle);
+
+            pitch = new JSlider(JSlider.HORIZONTAL, -50, 50, 0);
+            pitch.addChangeListener(new ChangeListener() {
+                @Override
+                public void stateChanged(ChangeEvent e) {
+                    JSlider source = (JSlider)e.getSource();
+                    if(tempPitch == source.getValue())
+                        pitch.setValue(0);
+                    else
+                        tempPitch = source.getValue();
+                }
+            });
+            pitch.setMajorTickSpacing(20);
+            pitch.setMinorTickSpacing(10);
+            pitch.setPaintTicks(true);
+            pitch.setPaintLabels(true);
+            pitch.setBounds(10, 90, 160, 50);
+            add(pitch);
+
+            yaw = new JSlider(JSlider.HORIZONTAL, -50, 50, 0);
+            yaw.addChangeListener(new ChangeListener() {
+                @Override
+                public void stateChanged(ChangeEvent e) {
+                    JSlider source = (JSlider)e.getSource();
+                    if(tempYaw == source.getValue())
+                        yaw.setValue(0);
+                    else
+                        tempYaw = source.getValue();
+                }
+            });
+            yaw.setMajorTickSpacing(20);
+            yaw.setMinorTickSpacing(10);
+            yaw.setPaintTicks(true);
+            yaw.setPaintLabels(true);
+            yaw.setBounds(10, 160, 160, 50);
+            add(yaw);
+
+            throttleLabel = new JLabel("Throttle");
+            throttleLabel.setBounds(70, 0, 100, 20);
+            add(throttleLabel);
+
+            pitchLabel = new JLabel("Pitch");
+            pitchLabel.setBounds(75, 70, 100, 20);
+            add(pitchLabel);
+
+            yawLabel = new JLabel("Yaw");
+            yawLabel.setBounds(77, 140, 100, 20);
+            add(yawLabel);
+        }
     }
 }
